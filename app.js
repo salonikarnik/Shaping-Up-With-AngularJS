62efe1b26bef741059fb1af728335fbe307c99bb
Saloni Karnik <salonikarnik21@gmail.com>
2017-02-24 18:50:29 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -83,5 +83,23 @@
         };
     });
 
+    app.directive('productPanels',function(){
+        return{
+            restrict:'E',
+            templateUrl:'product-panels.html',
+            controller:function(){
+                this.tab=1;
+                this.selectTab=function(setTab){
+                    this.tab=setTab;
+                };
+
+                this.isSelected=function(checkTab){
+                    return this.tab===checkTab;
+                };
+            },
+            controllerAs:'panel'
+        };
+    });
+
 
 })();
