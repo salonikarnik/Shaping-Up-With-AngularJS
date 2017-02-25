494b12c66a68c535ac7b7a63dc4be6d3416532d4
Saloni Karnik <salonikarnik21@gmail.com>
2017-02-24 19:01:43 -0600

    Level 4 Complete

--- app.js
+++ app.js
@@ -54,17 +54,7 @@
             ]
         }
     ];
-
-    app.controller('PanelController',function(){
-        this.tab=1;
-        this.selectTab=function(setTab){
-            this.tab=setTab;
-        };
-
-        this.isSelected=function(checkTab){
-            return this.tab===checkTab;
-        };
-    });
+    
 
     app.controller('ReviewController',function(){
         this.review={};
